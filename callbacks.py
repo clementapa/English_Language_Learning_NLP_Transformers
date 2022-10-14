8d14d3c60baa1d07aacb214b161bc6534c5e03ec
Clement Apavou <apavou.clement@outlook.fr>
2022-10-14 10:18:38 +0200

    Metric MCRMSE callback and implementation

---
+++ callbacks.py
@@ -0,0 +1,32 @@
+import pytorch_lightning as pl
+
+from metric import MCRMSE
+
+
+class MetricCallback(pl.Callback):
+    
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def on_fit_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        self.mcrmse_train = MCRMSE()
+        self.mcrmse_val = MCRMSE()
+
+    def on_validation_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", outputs, batch, batch_idx: int, dataloader_idx: int) -> None:
+        _, targets = batch
+        self.mcrmse_val(outputs['preds'], targets['labels'])
+
+    def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        mcrmse_val = self.mcrmse_val.compute()
+        self.mcrmse_val.reset()
+        self.log('val/mcrmse', mcrmse_val)
+
+    def on_train_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", outputs, batch, batch_idx: int, dataloader_idx: int) -> None:
+        _, targets = batch
+        self.mcrmse_train(outputs['preds'], targets['labels'])
+
+    def on_train_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        mcrmse_train = self.mcrmse_train.compute()
+        self.mcrmse_train.reset()
+        self.log('train/mcrmse', mcrmse_train)
+    
---
+++ metric.py
@@ -0,0 +1,20 @@
+from torchmetrics import Metric
+import torch
+
+class MCRMSE(Metric):
+    def __init__(self):
+        super().__init__()
+        self.add_state("mcrmse", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        assert preds.shape == target.shape
+
+        colwise_mse = torch.mean(torch.square(target - preds), dim=0)
+        loss = torch.mean(torch.sqrt(colwise_mse), dim=0)
+
+        self.mcrmse += loss.long()
+        self.total += target.shape[0]
+
+    def compute(self):
+        return self.mcrmse.float() / self.total
