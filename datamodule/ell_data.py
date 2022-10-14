9ee7c56bc5cc3f6b05263a7e5eea1b3191035fe1
Clement Apavou <apavou.clement@outlook.fr>
2022-10-14 10:19:22 +0200

    set  up dataset

--- datamodule/ell_data.py
+++ datamodule/ell_data.py
@@ -5,6 +5,7 @@ from transformers import AutoTokenizer
 import os.path as osp
 from sklearn.model_selection import train_test_split
 from utils import create_dir
+from datamodule.essay_dataset import EssayDataset
 
 class ELL_data(pl.LightningDataModule):
     def __init__(self, config):
@@ -13,42 +14,33 @@ class ELL_data(pl.LightningDataModule):
         self.root = config.root
         self.batch_size = config.batch_size
         self.num_workers = config.num_workers
+        self.config = config
 
         self.validation_split = config.validation_split
         self.tokenizer = AutoTokenizer.from_pretrained(config.name_model)
-
-    def tokenize_function(self, examples):
-        return self.tokenizer(examples["full_text"], padding="max_length", truncation=True)
-
-    def format_set(self, dataset):
-        tokenized_dataset = dataset.map(self.tokenize_function, batched=True)
-        tokenized_dataset.set_format(type='torch', columns=['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions', 'input_ids', 'token_type_ids', 'attention_mask'])
-        return tokenized_dataset
 
     def prepare_data(self) -> None:
         data_dir = osp.join('assets', f'dataset_train_val_{self.validation_split}.hf')
         if not osp.isdir(data_dir):
             dataset = load_dataset("csv", data_files=osp.join(self.root, "feedback-prize-english-language-learning/train.csv"))
             train, val = train_test_split(dataset['train'], test_size=self.validation_split, random_state=13)
-
-            create_dir(data_dir)
-            dataset['train'] = self.format_set(Dataset.from_dict(train))
-            dataset['val'] = self.format_set(Dataset.from_dict(val))
+            dataset['train'] = Dataset.from_dict(train)
+            dataset['val'] = Dataset.from_dict(val)
             self.dataset = dataset
             self.dataset.save_to_disk(data_dir)
         else:
             self.dataset = load_from_disk(data_dir)
-            self.dataset.set_format(type='torch', columns=['cohesion', 'syntax', 'vocabulary', 'phraseology', 'grammar', 'conventions', 'input_ids', 'token_type_ids', 'attention_mask'])
 
     def setup(self, stage=None):
         # split dataset
         if stage in (None, "fit"):   
-            self.train = self.dataset['train']
-            self.val = self.dataset['val']
-
+            self.train_set = EssayDataset(self.dataset['train'], self.config.max_length, tokenizer=self.tokenizer)
+            self.val_set = EssayDataset(self.dataset['val'], self.config.max_length, tokenizer=self.tokenizer)
+        else:
+            self.test_set = EssayDataset(self.dataset['test']) 
     def train_dataloader(self):
         train = DataLoader(
-            self.train,
+            self.train_set,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
             shuffle=True,
@@ -57,7 +49,7 @@ class ELL_data(pl.LightningDataModule):
 
     def val_dataloader(self):
         val = DataLoader(
-            self.val,
+            self.val_set,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
         )
---
+++ datamodule/essay_dataset.py
@@ -0,0 +1,38 @@
+import torch
+from operator import itemgetter
+
+class EssayDataset:
+    def __init__(self, df, max_length, tokenizer=None, is_test=False):
+        self.df = df
+        self.classes = ['cohesion','syntax','vocabulary','phraseology','grammar','conventions']
+        self.max_len = max_length
+        self.tokenizer = tokenizer
+        self.is_test = is_test
+        
+    def __getitem__(self,idx):
+        sample = self.df[idx]['full_text']
+        tokenized = self.tokenizer.encode_plus(sample,
+                                          None,
+                                          add_special_tokens=True,
+                                          max_length=self.max_len,
+                                          truncation=True,
+                                          padding='max_length'
+                                         )
+        inputs = {
+            "input_ids": torch.tensor(tokenized['input_ids'], dtype=torch.long),
+            "token_type_ids": torch.tensor(tokenized['token_type_ids'], dtype=torch.long),
+            "attention_mask": torch.tensor(tokenized['attention_mask'], dtype=torch.long)
+        }
+        
+        if self.is_test == True:
+            return inputs
+        
+        label = itemgetter(*self.classes)(self.df[idx])
+        targets = {
+            "labels": torch.tensor(label, dtype=torch.float32),
+        }
+        
+        return inputs, targets
+    
+    def __len__(self):
+        return len(self.df)
