99e8e8f4b700beb690538ae7a7742f4d127d1214
Clement Apavou <apavou.clement@outlook.fr>
2022-10-14 10:36:21 +0200

    freeze backbone true by default

--- model/multiregression.py
+++ model/multiregression.py
@@ -15,6 +15,7 @@ class MultiRegression(pl.LightningModule):
 
         # freeze backbone for fine tuned
         if config.freeze_backbone:
+            print('Freezing features extractor')
             for param in self.model.features_extractor.base_model.parameters():
                 param.requires_grad = False
 
